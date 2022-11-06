ff2ae38df285fbe6ae0a1d14f44cb824e7df85e9
Fractal-Tess <vgfractal@gmail.com>
2022-11-06 16:06:04 +0200

    first commit

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Button } from "ui";
+import { Button } from 'package-ui';
+import { themes } from 'package-types';
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      {themes}
     </div>
   );
 }
